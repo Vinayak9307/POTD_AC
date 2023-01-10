4b3561b4af6b9fc10b34c4b72737da91d5ab709f
Vinayak9307 <kushwahavinayak286@gmail.com>
2023-01-10 13:18:18 +0530

    Task 1 done

---
+++ 10JAN2023/q1.cpp
@@ -0,0 +1,52 @@
+// 10 jan 2023 
+// task 1 : Wave array
+// Time complexity : O(N)
+// Space complexity : O(1)
+// Link : https://practice.geeksforgeeks.org/problems/wave-array-1587115621/1?fbclid=IwAR1qDoAeLTDfOA3kNEz3Xn3LxBxqUJpeh--txw8AFfywXAd9yfnYjV0dhCA
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+// #include <algorithm>
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to sort the array into a wave-like array.
+    void convertToWave(int n, vector<int>& a){
+        
+        // Your code here
+        for(int i = 1 ; i < n ; i+=2){
+            int temp = a[i];
+            a[i] = a[i-1];
+            a[i-1] = temp;
+        }
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t; //Input testcases
+    while(t--) //While testcases exist
+    {
+        cin>>n; //input size of array
+        vector<int> a(n); //declare vector of size n
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //input elements of array
+        Solution ob;
+        ob.convertToWave(n, a);
+
+        for(int i=0;i<n;i++)
+            cout<<a[i]<<" "; //print array
+            
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
